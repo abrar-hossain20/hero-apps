9b17c1cfe3519a21aa8cfebb1c654a810c082ecc
Abrar200105 <abrar1224noyon@gmail.com>
2025-10-09 11:25:57 +0600

    basic setup done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ function App() {
 
   return (
     <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="mt-4 text-lg">Welcome to my app!</p>
     </>
   )
 }
